626539477557d58865f3b514ea181218c848af9a
Yuvraj-Dhepe <yuvi.kiit@gmail.com>
2023-04-16 22:14:37 +0200

    Creation of Gradio App

--- application.py
+++ application.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 from src.exception import CustomException
 from src.logger import logging
